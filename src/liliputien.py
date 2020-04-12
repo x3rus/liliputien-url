3a73a64ce1f326db4733f5b3cfad2c65134ce211
Thomas Boutry <thomas.boutry@x3rus.com>
2020-04-12 17:34:36 -0400

    #2 ; add urlValidation with unittest

--- src/liliputien.py
+++ src/liliputien.py
@@ -6,31 +6,33 @@ import pymongo
 import random
 import string
 import urllib.parse       # for mongodb password
+from urllib.parse import urlparse
 
 # self.bd
 # self.auth : {'user': "oo" , 'password' : "aaa"}
 
 # connection
 # write entry
-    # entry not already there
-    # validate value before
-    # catch BD exception
+#    # entry not already there
+#    # validate value before
+#    # catch BD exception
 # search entry
-    # manage two resultat choose
-    # catch BD exception
+#    # manage two resultat choose
+#    # catch BD exception
 
 
 class liliputien():
     """backend class, store urls and business logic"""
     def __init__(self, dbLocator='mongodb://%s:%s@localhost:27017/', databaseName="liliputien",
-                 collection="dbUrl", user="", passwd=""):
+                 collection="dbUrl", user="", passwd="", idSize=6):
         """Init liliputien"""
-        self.dbLocator = dbLocator
-        self.user = urllib.parse.quote_plus(user)
-        self.passwd = urllib.parse.quote_plus(passwd)
-        self.collection = collection
         self.databaseName = databaseName
+        self.dbLocator = dbLocator
         self.dbUrls = None
+        self.collection = collection
+        self.idSize = idSize
+        self.passwd = urllib.parse.quote_plus(passwd)
+        self.user = urllib.parse.quote_plus(user)
 
     def connectDb(self, connectTimeoutMS=20000, serverSelectionTimeoutMS=30000):
         """Establish the DB connection.
@@ -52,5 +54,31 @@ class liliputien():
 
         return None
 
-    def getRandomURLId(self, size=6, chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
+    def getRandomURLId(self, size=None, chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
+        """ Generate the random ID /fk8dS3 """
+        if size is None:
+            size = self.idSize
         return ''.join(random.choice(chars) for _ in range(size))
+
+    def uriValidator(self,url):
+        try:
+            result = urlparse(url)
+            return all([result.scheme, result.netloc])
+        except:
+            return False
+
+    def addUrlRedirection(self, urlDestination):
+        """add an entry in mongodb, validate the entry
+            return : urlID
+        """
+
+        if not self.uriValidator(urlDestination):
+            return None
+
+        # validate urlDestination
+        # get unid and confirme it
+        # write it 
+        # return the ID 
+        urlID = None
+        urlID = self.getRandomURLId()
+        return urlID
--- src/liliputien_test.py
+++ src/liliputien_test.py
@@ -19,16 +19,34 @@ class liliputienTest(unittest.TestCase):
         """Test if a subset of generated id are really uniq """
         backend = liliputien.liliputien()
 
-        randomListRnd= {}
+        randomListRnd = {}
         countRnd = 0
-        for x in range(1,100000):
+        for x in range(1, 100000):
             zeIdRnd = backend.getRandomURLId()
             if zeIdRnd in randomListRnd.keys():
                 # print("ERROR, key already there for Rnd")
                 countRnd += 1
             else:
-                randomListRnd[zeIdRnd]="ok"
-        self.assertEqual(countRnd,0,"No duplicate ID")
+                randomListRnd[zeIdRnd] = "ok"
+        self.assertEqual(countRnd, 0, "No duplicate ID")
+
+    def test_UrlValidation_True(self):
+        """Test method when you want add an URL"""
+        backend = liliputien.liliputien()
+        lstUrl2Validate = ['http://www.google.com/',
+                           'https://www.x3rus.com',
+                           'http://www.cwi.nl:80/%7Eguido/Python.html',
+                           'https://www.linuxfr.org/news/']
+        for url in lstUrl2Validate:
+            urlIsValid = backend.uriValidator(url)
+            self.assertTrue(urlIsValid)
+
+    def test_addUrlRedirection_True(self):
+        """Test method when you want add an URL"""
+        backend = liliputien.liliputien()
+        idURL = backend.addUrlRedirection("www.google.com")
+        # TODO add assert regex validating the idUrl
+        # self.assertIsNotNone(idURL)
 
 
 if __name__ == '__main__':
