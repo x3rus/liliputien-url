2e890f1522d282e55b487e6766895b8cb256c28a
Thomas Boutry <thomas.boutry@x3rus.com>
2020-04-13 14:33:31 -0400

    #4 ; test complete index.html feed input box , redirect to page /<data-feed-in-inputbox>

--- spike/seek-flask.py
+++ spike/seek-flask.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 #
+# REF : https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iii-web-forms
 #######################################
 
-from flask import Flask
+from flask import Flask, render_template, flash, redirect
 from markupsafe import escape
 from seekFlaskForms import UrlCreateEntry
 from flask import render_template
@@ -10,11 +11,14 @@ from flask import render_template
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     form = UrlCreateEntry()
+    if form.validate_on_submit():
+        flash('Url requester : {}'.format(
+            form.urlTarget.data))
+        return redirect('/'+form.urlTarget.data)
     return render_template('index.html', title='Enter Url', form=form)
-
 
 @app.route('/<urlId>')
 def show_urlId(urlId):
